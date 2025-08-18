ed7736e3b5356fedd592b621b34933ed300ebdbe
Taty <156003534+Taty1305@users.noreply.github.com>
2025-08-18 15:26:54 +0000

    alterando

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return <h1>My Darling,Victoria, eu Amo vc. Se vc me Ama da uma Risadinha.sua Tata</h1>
+    return <h1>My Darling sister,Victoria, eu Amo vc. Se vc me Ama da uma Risadinha. 
+        Tata :) </h1>
 
 }
 export default Home;
